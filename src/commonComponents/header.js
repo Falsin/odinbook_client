bc2bf867b18a3458165d48cd9c34d62a20768e9e
Falsin <greenldend@gmail.com>
2022-09-19 21:35:51 +0700

    добавил метод logout и вывод информации о пользователе если предоставлены учётные данные

--- src/commonComponents/header.js
+++ src/commonComponents/header.js
@@ -3,18 +3,39 @@ import styled from "styled-components";
 import CommonContext from "../commonContext";
 
 function Header({className, children}) {
+  async function logOut(context) {
+    const request = await fetch(context.commonInfo.serverLink + 'logout', {
+      credentials: "include",
+    })
+    const response = await request.json();
+
+    context.setCommonInfo({
+      ...context.commonInfo,
+      credential: response
+    })
+  }
+
   return (
     <CommonContext.Consumer>
       {(context) => {
         return (
           <header className={className}>
             <h1>Odinbook</h1>
-            <h1>{context.commonInfo.credential.username}</h1>
+            {(() => {
+              if (context.commonInfo.credential) {
+                return (
+                  <div>
+                    <h1>{context.commonInfo.credential.username}</h1>
+                    <button onClick={() => {
+                      logOut(context);
+                    }}>Log out</button>
+                  </div>
+                )
+              }
+            })()}
           </header>
         )
-      }
-
-      }
+      }}
     </CommonContext.Consumer>
   )
 }
@@ -22,7 +43,17 @@ function Header({className, children}) {
 const StyledHeader = styled(Header)`
   background: rgb(56, 56, 56);
   color: white;
-  padding: 2vmin;
+  padding: 2vmin 5vmin 2vmin 5vmin;
+  display: flex;
+  justify-content: space-between;
+
+  & div {
+    display: flex;
+
+    h1 {
+      margin-right: 2vmin;
+    }
+  }
 `
 
 export default StyledHeader;
