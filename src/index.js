b1f35b4b90d295b1d857ac03691464da6a4a323f
Falsin <greenldend@gmail.com>
2022-09-19 21:31:32 +0700

    убрал строгий мод

--- src/index.js
+++ src/index.js
@@ -6,8 +6,4 @@ import './styles/style.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <React.StrictMode>
-    <RouteSwitch />
-  </React.StrictMode>
-)
+root.render(<RouteSwitch />)
