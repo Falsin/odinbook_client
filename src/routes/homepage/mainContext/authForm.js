6a380955cfadd4ac64112a1fb99065e19ccbd0e1
Falsin <greenldend@gmail.com>
2022-09-20 21:29:24 +0700

    Создал фунцию submit проверящую поля формы на заполненность и отправляющую запрос на сервер.

--- src/routes/homepage/mainContext/authForm.js
+++ src/routes/homepage/mainContext/authForm.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import uniqid from "uniqid";
+import CommonContext from "../../../commonContext";
 
 function AuthForm({className, children, changeMode}) {
+  const [credentials, setCredentials] = useState({});
+  const [inputElementsStatus, setInputElementsStatus] = useState({
+    username: true,
+    password: true
+  });
+
   let id;
 
   function setUniqId() {
@@ -10,23 +17,87 @@ function AuthForm({className, children, changeMode}) {
     return id;
   }
 
+  function changeCredentials(e) {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  async function submit(e, context) {
+    e.preventDefault();
+
+    const arrayKeys = Object.keys(inputElementsStatus);
+
+    if (arrayKeys.every(item => credentials[item])) {
+      const request =  await fetch(context.commonInfo.serverLink + "login", {
+        credentials: "include",
+        body: JSON.stringify(credentials),
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+
+      const response = await request.json();
+  
+      if (Array.isArray(response)) {
+        response.forEach(item => console.log(item.msg))
+      } else {
+        console.log(response)
+        context.setCommonInfo({
+          ...context.commonInfo,
+          credential: response
+        })
+      }
+    } else {
+      const objecStatus = arrayKeys.reduce((prevVal, currVal) => {
+
+        if (credentials[currVal]) {
+          return {
+            ...prevVal,
+            [currVal]: true,
+          }
+        } else {
+          return {
+            ...prevVal,
+            [currVal]: false,
+          }
+        }
+      }, {})
+
+      console.log(objecStatus)
+
+      setInputElementsStatus(objecStatus)
+    }
+  }
+
   return (
-    <form className={className}>
-      <div className="field">
-        <label htmlFor={setUniqId()}>Login</label>
-        <input id={id} type="text" />
-      </div>
+    <CommonContext.Consumer>
+      {context => {
+        return (
+          <form className={className} onSubmit={e => submit(e, context)}>
+            <div className="field">
+              <label htmlFor={setUniqId()}>Username</label>
+              <input id={id} type="text" name="username"
+                onChange={changeCredentials} className={inputElementsStatus.username ? "" : "error"}
+              />
+            </div>
 
-      <div className="field">
-        <label htmlFor={setUniqId()}>Password</label>
-        <input id={id} type="password" />
-      </div>
+            <div className="field">
+              <label htmlFor={setUniqId()}>Password</label>
+              <input id={id} type="password" name="password" 
+              onChange={changeCredentials} className={inputElementsStatus.password ? "" : "error"} />
+            </div>
 
-      <button>Log in</button>
-      <button type="button" onClick={() => changeMode(true)}>Create new account</button>
-      <button type="button">Test drive an existing account</button>
-      <button>Log in with Facebook</button>
-    </form>
+            <button>Log in</button>
+            <button type="button" onClick={() => changeMode(true)}>Create new account</button>
+            <button type="button">Test drive an existing account</button>
+            <button>Log in with Facebook</button>
+          </form>
+        )
+      }}
+    </CommonContext.Consumer>
   )
 }
 
@@ -34,9 +105,11 @@ const StyledAuthForm = styled(AuthForm)`
   display: flex;
   flex-direction: column;
   width: 50%;
+  max-width: 400px;
 
   .field {
     display: flex;
+    margin-top: 2vmin;
 
     label {
       min-width: 65px;
@@ -45,8 +118,24 @@ const StyledAuthForm = styled(AuthForm)`
 
     input {
       flex-grow: 1;
+
+      &.error {
+        background: red;
+      }
     }
   }
+
+  button {
+    margin-top: 2vmin;
+
+    &:first-of-type {
+      margin-top: 3vmin;
+    }
+  }
+
+/*   button:first-child {
+      margin-top: 3vmin;
+    } */
 `
 
 export default StyledAuthForm;
