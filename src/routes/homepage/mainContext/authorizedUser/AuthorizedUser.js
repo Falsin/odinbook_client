0e0610d78622f5429359e6fa0eb8cc1e2cea04e8
Falsin <greenldend@gmail.com>
2022-10-02 14:51:06 +0700

    создал компонент для авторизованного пользователя

---
+++ src/routes/homepage/mainContext/authorizedUser/AuthorizedUser.js
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from "styled-components";
+
+function AuthorizedUser({className, children}) {
+  return (
+    <div id="authorizedUser" className={className}>
+      <div id="account">
+        <ul>
+          <li>Logo and Full name</li>
+          <li>Friends</li>
+          <li>Settings</li>
+        </ul>
+      </div>
+
+      <div id="news">
+        <ul>
+          <li>News 1</li>
+          <li>News 2</li>
+          <li>News 3</li>
+        </ul>
+      </div>
+
+      <div id="friends">
+        <ul>
+          <li>
+            <h2>Incoming friends requests</h2>
+            <ul>
+              <li>1 Friend</li>
+              <li>2 Friend</li>
+              <li>3 Friend</li>
+            </ul>
+          </li>
+
+          <li>
+            <h2>Outcoming friends requests</h2>
+            <ul>
+              <li>1 Friend</li>
+              <li>2 Friend</li>
+              <li>3 Friend</li>
+            </ul>
+          </li>
+
+          <li>
+            <h2>Friends</h2>
+            <ul>
+              <li>1 Friend</li>
+              <li>2 Friend</li>
+              <li>3 Friend</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+const StyledAuthorizedUser = styled(AuthorizedUser)`
+  background: #616161;
+  color: white;
+  display: flex;
+  padding: 5vmin;
+  position: relative;
+
+  #news {
+      width: 50%;
+    }
+    #account, #friends {
+      width: 25%;
+    }
+
+    ul {
+      list-style-type: none;
+    }
+`
+
+export default StyledAuthorizedUser;
