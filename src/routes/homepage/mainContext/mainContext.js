7d933cc283456e08ca2adf1774b730da926cbf51
Falsin <greenldend@gmail.com>
2022-10-02 14:44:45 +0700

    разделил mainContext на два компонента

--- src/routes/homepage/mainContext/mainContext.js
+++ src/routes/homepage/mainContext/mainContext.js
@@ -1,42 +1,19 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import CommonContext from "../../../commonContext";
-import StyledAuthForm from "./authForm/authForm";
-import StyledRegistForm from "./registForm";
-
+import StyledUnauthorizedUser from "./unauthorizedUser/UnauthorizedUser";
+import StyledAuthorizedUser from "./authorizedUser/AuthorizedUser";
 
 function MainContext({className, children}) {
-  const [isRegistMode, setRegistMode] = useState(false);
-
   return (
     <CommonContext.Consumer>
       {(context) => {
-        return (
-          <div className={className}>
-            <div id="about">
-              <h2>Odinbook</h2>
-              <p>Connect with friends and the world around you on Odinbook.</p>
-            </div>
-            <StyledAuthForm changeMode={setRegistMode} />
-            {isRegistMode ? <StyledRegistForm changeMode={setRegistMode} /> : null}
-          </div>
-        )
-      }
-      }
+        return !context.commonInfo.credential 
+            ? <StyledUnauthorizedUser /> 
+            : <StyledAuthorizedUser />
+      }}
     </CommonContext.Consumer>
   )
 }
 
-const StyledMainContext = styled(MainContext)`
-  background: #616161;
-  color: white;
-  display: flex;
-  padding: 5vmin;
-  position: relative;
-
-  #about {
-    width: 50%;
-  }
-`
-
-export default StyledMainContext;
+export default MainContext;
