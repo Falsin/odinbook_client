c8185799fb54c3ee120b4aeb9ad24a02de0ffbea
Falsin <greenldend@gmail.com>
2022-10-02 11:24:15 +0700

    рефакторинг

--- src/routes/homepage/mainContext/authForm/authForm.js
+++ src/routes/homepage/mainContext/authForm/authForm.js
@@ -4,174 +4,17 @@ import CommonContext from "../../../../commonContext";
 import StyledField from "./field";
 import uniqid from "uniqid";
 
-/* function AuthForm({className, children, changeMode}) {
-  const [credentials, setCredentials] = useState({});
-  const [inputElementList] = useState(["username", "password"]);
-
-  async function submit(e, context) {
-    e.preventDefault();
-
-    const invalidKeys = inputElementList.filter(item => !credentials[item]);
-    let changedState;
-
-    if (!invalidKeys.length) {
-      const response = await request(context);
-
-      if (Array.isArray(response)) {
-        const changedReponse = response.map(item => item.param);
-        changedState = changeState(changedReponse);
-      } else {
-        context.setCommonInfo({
-          ...context.commonInfo,
-          credential: response
-        })
-      }
-    } else {
-      changedState = changeState(invalidKeys);
-    }
-
-    setCredentials(changedState);
-  }
-
-  function changeState(arrayElements) {
-    return arrayElements.reduce((prevVal, currVal) => {
-      return {
-        ...prevVal,
-        [currVal]: null
-      }
-    }, {...credentials})
-  }
-
-  console.log(credentials)
-
-  async function request(context) {
-    const request =  await fetch(context.commonInfo.serverLink + "login", {
-      credentials: "include",
-      body: JSON.stringify(credentials),
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-
-    return request.json();
-  }
-
-  return (
-    <CommonContext.Consumer>
-      {context => {
-        return (
-          <form className={className} onSubmit={e => submit(e, context)}>
-            {inputElementList.map((item, id) => {
-              return <StyledField key={id}
-                name={item} 
-                credentials={credentials} 
-                setCredentials={setCredentials}  
-                />
-            })}
-
-            <button>Log in</button>
-            <button type="button" onClick={() => changeMode(true)}>Create new account</button>
-            <button type="button">Test drive an existing account</button>
-            <a href={context.commonInfo.serverLink + "login/facebook"}>Log in with Facebook</a>
-          </form>
-        )
-      }}
-    </CommonContext.Consumer>
-  )
-} */
-
-/* function AuthForm({className, children, changeMode}) {
-  const [credentials, setCredentials] = useState({});
-  const [inputElementList] = useState(["username", "password"]);
-
-  async function submit(e, context) {
-    e.preventDefault();
-
-    const invalidKeys = inputElementList.filter(item => !credentials[item]);
-    let changedState;
-
-    if (!invalidKeys.length) {
-      const response = await request(context);
-
-      if (Array.isArray(response)) {
-        const changedReponse = response.map(item => item.param);
-        changedState = changeState(changedReponse);
-      } else {
-        setCredentials({}) 
-        console.log('изменяю состояние родителя')
-        // в будущем это изменение состояния можно убрать, 
-        //так как при нажатии на форму будет происходить какое-то действие
-        //например, переход на новую страницу
-        return context.setCommonInfo({
-          ...context.commonInfo,
-          credential: response
-        })
-      }
-    } else {
-      changedState = changeState(invalidKeys);
-    }
-    setCredentials(changedState);
-  }
-
-  function changeState(arrayElements) {
-    return arrayElements.reduce((prevVal, currVal) => {
-      return {
-        ...prevVal,
-        [currVal]: null
-      }
-    }, {...credentials})
-  }
-
-  async function request(context) {
-    const request =  await fetch(context.commonInfo.serverLink + "login", {
-      credentials: "include",
-      body: JSON.stringify(credentials),
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-
-    return request.json();
-  }
-
-  return (
-    <CommonContext.Consumer>
-      {context => {
-        return (
-          <form className={className} onSubmit={e => submit(e, context)}>
-            {inputElementList.map((item, id) => {
-              return <StyledField key={uniqid()}
-                name={item} 
-                credentials={credentials} 
-                setCredentials={setCredentials}  
-                />
-            })}
-
-            <button>Log in</button>
-            <button type="button" onClick={() => changeMode(true)}>Create new account</button>
-            <button type="button">Test drive an existing account</button>
-            <a href={context.commonInfo.serverLink + "login/facebook"}>Log in with Facebook</a>
-          </form>
-        )
-      }}
-    </CommonContext.Consumer>
-  )
-} */
-
 function AuthForm({className, children, changeMode}) {
-  /* const [credentials, setCredentials] = useState({});
-  const [inputElementList] = useState(["username", "password"]); */
-  const [credentials, setCredentials] = useState({
+  const defaultState = {
     username: null,
     password: null
-  });
+  }
+
+  const [credentials, setCredentials] = useState({...defaultState});
 
   async function submit(e, context) {
     e.preventDefault();
 
-    //const invalidKeys = inputElementList.filter(item => !credentials[item]);
     const invalidKeys = Object.values(credentials).filter(item => !item);
     let changedState;
 
@@ -182,10 +25,7 @@ function AuthForm({className, children, changeMode}) {
         const changedReponse = response.map(item => item.param);
         changedState = changeState(changedReponse);
       } else {
-        setCredentials({
-          username: null,
-          password: null
-        }) 
+        setCredentials({...defaultState}) 
         return context.setCommonInfo({
           ...context.commonInfo,
           credential: response
@@ -205,15 +45,6 @@ function AuthForm({className, children, changeMode}) {
       }
     }, credentials)
   }
-
-  /* function changeState(arrayElements) {
-    return arrayElements.reduce((prevVal, currVal) => {
-      return {
-        ...prevVal,
-        [currVal]: null
-      }
-    }, {...credentials})
-  } */
 
   async function request(context) {
     const request =  await fetch(context.commonInfo.serverLink + "login", {
@@ -240,13 +71,6 @@ function AuthForm({className, children, changeMode}) {
                 setCredentials={setCredentials}  
                 />
             })}
-            {/* {inputElementList.map((item, id) => {
-              return <StyledField key={uniqid()}
-                name={item} 
-                credentials={credentials} 
-                setCredentials={setCredentials}  
-                />
-            })} */}
 
             <button>Log in</button>
             <button type="button" onClick={() => changeMode(true)}>Create new account</button>
--- src/routes/homepage/mainContext/authForm/field.js
+++ src/routes/homepage/mainContext/authForm/field.js
@@ -2,78 +2,27 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import uniqid from "uniqid";
 
-/* function Field({name, credentials, setCredentials, className, children}) {
-  let id;
-  const [isError, setIsErrorStatus] = useState(false);
-
-  useEffect(() => {
-    setIsErrorStatus((!credentials || credentials[name] === null) ? true : false)
-  })
-
-  function setUniqId() {
-    id = uniqid();
-    return id;
-  }
-
-  function changeCredentials(e) {
-    setCredentials({
-      ...credentials,
-      [e.target.name]: e.target.value.trim()
-    })
-  }
-
-  function Blur(e) {
-    if (e.target.value === "") {
-      setCredentials({
-        ...credentials,
-        [e.target.name]: null
-      })
-    }
-  }
-
-  return (
-    <div className={className}>
-      <label htmlFor={setUniqId()}>{name}</label>
-        <div className={isError ? "error" : ""}>
-          <input id={id} type={name == "password" ? "password" : "text"} name={name} 
-            onChange={changeCredentials} onBlur={Blur} />
-        </div>
-    </div>
-  )
-} */
-
 function Field({name, credentials, setCredentials, className, children}) {
   let id;
   const [isError, setIsErrorStatus] = useState(false);
 
-  const [testState, setTestState] = useState(credentials[name])
-
   useEffect(() => {
-    setTestState(credentials[name])
-    setIsErrorStatus(testState === "" ? true : false)
+    setIsErrorStatus(credentials[name] === "" ? true : false)
   })
-
-  useEffect(() => {
-    console.log("компонент смонтировался")
-  }, [])
 
   function setUniqId() {
     id = uniqid();
     return id;
   }
-
-  console.log(testState)
 
   function changeCredentials(e) {
     setCredentials({
       ...credentials,
       [name]: e.target.value
     })
-    setTestState(e.target.value)
   }
 
   function Blur(e) {
-    setTestState(e.target.value)
     setCredentials({
       ...credentials,
       [name]: e.target.value
@@ -85,7 +34,7 @@ function Field({name, credentials, setCredentials, className, children}) {
       <label htmlFor={setUniqId()}>{name}</label>
         <div className={isError ? "error" : ""}>
             <input id={id} type={name == "password" ? "password" : "text"} name={name} 
-            onChange={changeCredentials} onBlur={Blur} value={testState ? testState : ""} />
+            onChange={changeCredentials} onBlur={Blur} value={credentials[name] ? credentials[name] : ""} />
         </div>
     </div>
   )
