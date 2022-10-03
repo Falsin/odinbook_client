07e5b18496031d95a1177b03c872b473493d394d
Falsin <greenldend@gmail.com>
2022-10-03 08:51:22 +0700

    добавил новые зависимости

--- src/routes/settingsPage/SettingsPage.js
+++ src/routes/settingsPage/SettingsPage.js
@@ -1,8 +1,15 @@
 import React from "react";
+import StyledFooter from "../../commonComponents/footer";
+import StyledHeader from "../../commonComponents/header";
+import MainContext from "./MainContext";
 
 function SettingsPage(params) {
   return (
-    <div>This is the settings page</div>
+    <>
+      <StyledHeader />
+      <MainContext />
+      <StyledFooter />
+    </>
   )
 }
 
