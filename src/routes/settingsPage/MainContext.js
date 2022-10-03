963cb21c77ed7458924e7b9beb80d8cb403598b1
Falsin <greenldend@gmail.com>
2022-10-03 09:50:34 +0700

    реализовал функцию удаления

--- src/routes/settingsPage/MainContext.js
+++ src/routes/settingsPage/MainContext.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import CommonContext from "../../commonContext";
 
 function MainContext() {
+  const navigate = useNavigate();
+
+  async function deleteAccount(context) {
+    const request = await fetch(context.commonInfo.serverLink + "account", {
+      body: JSON.stringify(context.commonInfo.credential),
+      method: "DELETE",
+      credentials: "include"
+    })
+    const response = await request.json();
+
+    if (response) {
+      navigate("/")
+    }
+  }
+
   return (
     <CommonContext.Consumer>
       {(context) => {
@@ -11,7 +27,7 @@ function MainContext() {
             <div id="account">
               <img></img>
               <div>Name</div>
-              <button>Delete account</button>
+              <button onClick={() => deleteAccount(context)}>Delete account</button>
             </div>
           </div>
         )
